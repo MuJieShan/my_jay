61cc3f0c9925165aa7c06b9fc1858c801c883580
MuJieShan <2902005603@qq.com>
2025-02-26 20:24:21 +0800

    1

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # from transformers import pipeline
-a=0.00000002
-b=0.00000001
-c=b*7
-r=[i*b for i in range(2,16)]
-print(a,b,c)
+a=1.0e-7
+b=1.0e-8
+c=a+2*b
+d=1.1e-7
+r=[a+i*b for i in range(0,10)]
+print(a,b,c,d)
 print(r)
