61ccdec4741315dab624fb47c57791221c5e1272
MuJieShan <2902005603@qq.com>
2025-02-26 18:18:22 +0800

    1

--- lp/lp.sh
+++ lp/lp.sh
@@ -1,4 +1,4 @@
 #bin/bash
-export _HF_DEFAULT_ENDPOINT="https://hf-mirror.com"
-export HF_ENDPOINT="https://hf-mirror.com"
+#export _HF_DEFAULT_ENDPOINT="https://hf-mirror.com"
+#export HF_ENDPOINT="https://hf-mirror.com"
 python ../train.py --dataset mrpc --seed 3404 --epoch 1 --reg 5e-08 --weight_decay 0.001 --model bert-base-uncased --batchsize 32
