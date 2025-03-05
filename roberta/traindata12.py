6acaa0dc8aedf0c8f4635500b0dad5323736b178
MuJieShan <2902005603@qq.com>
2025-03-05 15:32:27 +0800

    1

--- roberta/traindata12.py
+++ roberta/traindata12.py
@@ -288,6 +288,6 @@ def main():
 if __name__ == "__main__":
     main()
     #剪枝标准：
-            #初始模型对样本的损失颗粒的绝对值
+            #模型对样本的损失颗粒的绝对值
     #测试集样本在微调前后的颗粒状况
     # python ../traindata12.py --state ft --dataset mrpc --seed 3404 --reg 5e-8 --weight_decay 0.0 --epoch 10 --remain_loss 1 --model bert-base-uncased --target_ratio 0.5 --batchsize 32 --pruneFlag up --optim adamw_torch
