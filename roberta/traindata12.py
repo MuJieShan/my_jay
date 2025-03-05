ea863f36ab4b20b716909938d4a6b1c0a040e976
MuJieShan <2902005603@qq.com>
2025-03-05 15:31:58 +0800

    1

--- roberta/traindata12.py
+++ roberta/traindata12.py
@@ -27,21 +27,75 @@ def main():
         remain_loss = True
     #Load model and tokenizer
     model,tokenizer = get_model_and_tokenizer(model_checkpoint,task,device)
+    # Load DataLoader
+    print(f"\nLoading data...")
+    train_dataloader, eval_dataset, trainset = get_dataloader3(task, model_checkpoint, tokenizer=tokenizer,shuffle=config.shuffle,batch_size=batch_size)
+    # data pruning
+    compress = config.reg
+    data_p = GLUEPruner(dataset=trainset, ratio=config.target_ratio, pruneFlag=config.pruneFlag)
+    data_p.prune()
+    sampler = data_p.get_sampler()
+    train_epoch_iterator = train_dataloader
+    print("开始预先训练")
+    train_dataset = trainset
+    data_collator = DataCollatorWithPadding(tokenizer)
+    eval_steps = len(train_epoch_iterator) // 2
+    # 定义训练参数
+    training_args = GlueTrainingArguments(
+        state=config.state,
+        # training_args
+        seed=config.seed,
+        learning_rate=config.learning_rate,
+        lr_scheduler_type="linear",
+        num_train_epochs=config.epoch0,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        warmup_ratio=0.1,
+        # warmup_steps=50,
+        weight_decay=config.weight_decay,
+        do_train=True,
+        reg=config.reg,
+        task_name=task,
+        shuffle=config.shuffle,
+        optim=config.optim,
+
+        # eval_args
+        eval_strategy="steps",
+        eval_steps=eval_steps,  # "steps","epoch"# eval_steps=50,
+        save_strategy="steps",
+        save_steps=eval_steps,
+        save_only_model=True,
+        metric_for_best_model=GLUE_METRIC[task],
+        greater_is_better=True,
+        save_safetensors=False,
+        # logging_args
+        output_dir=f"./log/model/{config.dataset}_{config.seed}_{config.pruneFlag}_{config.target_ratio}_{config.weight_decay}_{config.reg}",
+        load_best_model_at_end=True,
+        report_to=["tensorboard"],
+    )
+    # del model,tokenizer
+    # 创建Trainer实例
+    trainer = GlueTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=data_collator,
+        compute_metrics=lambda eval_pred: compute_metrics(eval_pred, task),
+    )
+    trainer.train()
+    s = f'{trainer.evaluate(eval_dataset)}'
+    print(s)
+    log.info(f'预先训练{s}')
+    print("结束预先训练")
     model1, _ = get_model_and_tokenizer(model_checkpoint, task, device)
     model1.load_state_dict(copy.deepcopy(model.state_dict()))
     model1.to(next(model.parameters()).device)
     model2, _ = get_model_and_tokenizer(model_checkpoint, task, device)
     model2.load_state_dict(copy.deepcopy(model.state_dict()))
     model2.to(next(model.parameters()).device)
-    # Load DataLoader
-    print(f"\nLoading data...")
-    train_dataloader, eval_dataset, trainset = get_dataloader3(task, model_checkpoint, tokenizer=tokenizer,shuffle=config.shuffle,batch_size=batch_size)
-    # data pruning
-    compress = config.reg
-    data_p = GLUEPruner(dataset=trainset, ratio=config.target_ratio,pruneFlag=config.pruneFlag)
-    data_p.prune()
-    sampler = data_p.get_sampler()
-    train_epoch_iterator = train_dataloader
+
+
     loss_before = {}
     loss_after = {}
     iterator = iter(train_epoch_iterator)
