ecb38b47f97d7f61a69172f6fa3ab5557de68ddc
MuJieShan <2902005603@qq.com>
2025-03-05 15:01:39 +0800

    1

--- utils.py
+++ utils.py
@@ -2089,7 +2089,8 @@ def  train_lp_loop1(config, model, train_epoch_iterator,train_dataloader1,eval_e
     def loss_particles(model_lp,e,loss_gap):
         loss_g_before = {}
         iterator = iter(train_dataloader1)
-        trange = range(len(train_dataloader1))
+        # trange = range(len(train_dataloader1))
+        trange = range(1000)
         before = tqdm(total=len(train_dataloader1), desc=f"lp before{e}")
         for step in trange:
             before.update(1)
@@ -2110,7 +2111,8 @@ def  train_lp_loop1(config, model, train_epoch_iterator,train_dataloader1,eval_e
 
         loss_g_after = {}
         iterator = iter(train_dataloader1)
-        trange = range(len(train_dataloader1))
+        # trange = range(len(train_dataloader1))
+        trange = range(1000)
         after = tqdm(total=len(train_dataloader1), desc=f"lp after{e}")
         for step in trange:
             after.update(1)
