fd71bab02d8ca2a39939f03d3018b0d915b4289f
MuJieShan <2902005603@qq.com>
2025-03-07 13:43:26 +0800

    1

--- roberta/loss/0/3404_0.sh
+++ roberta/loss/0/3404_0.sh
@@ -14,10 +14,6 @@ task=('mnli')
 for t in "${task[@]}"
 do
     echo "运行task=$t"
-    python_command="python ../../traindata13_loss.py --state ft --dataset $t --seed 3404 --reg 5e-8 --weight_decay $weight_decay --epoch0 1 --epoch $epoch --remain_loss 1 --model $model --target_ratio $target_ratio --pruneFlag up --batchsize $batchsize --learning_rate $learning_rate"
-    eval $python_command
-    python_command="python ../../traindata13_loss.py --state ft --dataset $t --seed 3404 --reg 5e-8 --weight_decay $weight_decay --epoch0 1 --epoch $epoch --remain_loss 1 --model $model --target_ratio $target_ratio --pruneFlag down --batchsize $batchsize --learning_rate $learning_rate"
-    eval $python_command
     python_command="python ../../traindata13_loss.py --state ft --dataset $t --seed 3404 --reg 5e-8 --weight_decay $weight_decay --epoch0 0 --epoch $epoch --remain_loss 1 --model $model --target_ratio $target_ratio --pruneFlag up --batchsize $batchsize --learning_rate $learning_rate"
     eval $python_command
     python_command="python ../../traindata13_loss.py --state ft --dataset $t --seed 3404 --reg 5e-8 --weight_decay $weight_decay --epoch0 0 --epoch $epoch --remain_loss 1 --model $model --target_ratio $target_ratio --pruneFlag down --batchsize $batchsize --learning_rate $learning_rate"
